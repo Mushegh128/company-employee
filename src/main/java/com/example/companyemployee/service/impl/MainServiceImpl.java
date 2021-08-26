1407c23749ebfee4f6e1a39cac12983b0136b9b4
Mushegh128 <thesoceco@gmail.com>
2021-08-26 18:15:01 +0400

    hw M1

---
+++ src/main/java/com/example/companyemployee/service/impl/MainServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.companyemployee.service.impl;
+
+import com.example.companyemployee.model.Company;
+import com.example.companyemployee.model.Employee;
+import com.example.companyemployee.repository.CompanyRepository;
+import com.example.companyemployee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MainServiceImpl {
+
+    private final EmployeeRepository employeeRepository;
+    private final CompanyRepository companyRepository;
+
+    public Object findByEmail(String s){
+        Optional<Employee> employeeOpt = employeeRepository.findByEmail(s);
+        Optional<Company> companyOpt = companyRepository.findByEmail(s);
+
+        if (employeeOpt.isPresent()){
+            return employeeOpt.get();
+        }else if(companyOpt.isPresent()){
+            return companyOpt.get();
+        }else return null;
+    }
+
+}
